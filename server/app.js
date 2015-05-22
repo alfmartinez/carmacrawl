145ed877881f3192039889a246df2a78abd5959e
Alexandre Martinez <alexandre.martinez@external.grdf.fr>
2015-05-22 14:17:15 +0200

    Game Loop initiated

--- server/app.js
+++ server/app.js
@@ -10,28 +10,34 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./config/environment');
+var loop = require('./gameloop/gameloop');
 
 // Connect to database
 mongoose.connect(config.mongo.uri, config.mongo.options);
 
 // Populate DB with sample data
-if(config.seedDB) { require('./config/seed'); }
+if (config.seedDB) {
+	require('./config/seed');
+}
 
 // Setup server
 var app = express();
 var server = require('http').createServer(app);
 var socketio = require('socket.io')(server, {
-  serveClient: (config.env === 'production') ? false : true,
-  path: '/socket.io-client'
+	serveClient: (config.env === 'production') ? false : true,
+	path: '/socket.io-client'
 });
 require('./config/socketio')(socketio);
 require('./config/express')(app);
 require('./routes')(app);
 
 // Start server
-server.listen(config.port, config.ip, function () {
-  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+server.listen(config.port, config.ip, function() {
+	console.log('Express server listening on %d, in %s mode', config.port, app.get(
+		'env'));
 });
+
+setInterval(loop.doLoop, 5000);
 
 // Expose app
 exports = module.exports = app;
---
+++ server/gameloop/gameloop.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports.doLoop = function() {
+	console.log('Turn');
+};
